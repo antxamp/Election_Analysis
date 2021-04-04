8ad52fe594256519f1b4a8c86f7795d1ebcc0731
AntxAmp <78179816+antxamp@users.noreply.github.com>
2021-04-03 20:38:53 -0500

    Adding PyPoll.py file.

---
+++ PyPoll.py
@@ -0,0 +1,46 @@
+import csv
+import os
+
+# # Assign a variable for the file to load and the path.
+
+file_to_load = os.path.join('Resources','election_results.csv')
+
+# # Open the election results and read the file.
+# with open(file_to_load) as election_data:
+
+#     # # # Print the file object.
+#     print(election_data)
+
+# Create a filename variable to a direct or indirect path to the file.
+# file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# # Using the open() function with the "w" mode we will write data to the file.
+# outfile = open(file_to_save, "w")
+
+# #Write some data to the file.
+# outfile.write("Arapahoe, ")
+# outfile.write("Denver, ")
+# outfile.write("Jefferson, ")
+
+# #Close the file
+# outfile.close()
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# # Using the with statement open the file as a text file.
+# with open(file_to_save, "w") as txt_file:
+
+#     # Write some data to the file.
+#     txt_file.write("Counties in the Election\n--------------\nArapahoe\nDenver\nJefferson")
+   
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+  # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+    # Print each row in the CSV file.
+    # for row in file_reader:
+    #     print(row)
+     # Print the header row.
+    headers = next(file_reader)
+    print(headers)
